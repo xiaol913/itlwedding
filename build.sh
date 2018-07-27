04ac29347741fa972852c5525284c71ec99980c9
Shawn Gao <xxlangdou@gmail.com>
2018-07-27 17:55:12 +0800

    fix bug

--- build.sh
+++ build.sh
@@ -7,48 +7,47 @@ echo '===== moving files ====='
 sleep 2
 cp -r ./db ../
 cp -r ./nginx ../
+cp -r ./sentry ../
 cp ./docker-compose.yml ../
 
 echo '===== creating netword ====='
 sleep 2
 docker network create shawnlive
-cd ..
-git clone https://github.com/getsentry/onpremise.git
-cd ./onpremise
+cd ../sentry
 mkdir -p data/{sentry,postgres}
 
-echo '===== building sentry images(1) ====='
-sleep 2
-docker-compose build
-key=`docker-compose run --rm web config generate-secret-key | tail -1`
+# echo '===== building sentry images(1) ====='
+# sleep 2
+# docker-compose build
+# key=`docker-compose run --rm web config generate-secret-key | tail -1`
 
-echo "===== the key is $key ====="
-sleep 2
-sed -i "/SENTRY_SECRET_KEY:/{s/''/'$key'/}" ./docker-compose.yml
+# echo "===== the key is $key ====="
+# sleep 2
+# sed -i "/SENTRY_SECRET_KEY:/{s/''/'$key'/}" ./docker-compose.yml
 
-echo '===== building sentry images(2) ====='
-sleep 2
-/bin/expect << -EOF
-    set timeout 30
-    spawn docker-compose run --rm web upgrade
-    expect {
-        "Would you like to create a user account now*" { send "y\r"; exp_continue }
-        "Email*" { send "admin@admin.com\r"; exp_continue } 
-        "Password*" { send "123456\r"; exp_continue }
-        "Repeat for confirmation*" { send "123456\r"; exp_continue }
-        "Should this user be a superuser*" { send "y\r" }
-    }
-    expect eof
--EOF
+# echo '===== building sentry images(2) ====='
+# sleep 2
+# /bin/expect << -EOF
+#     set timeout 30
+#     spawn docker-compose run --rm web upgrade
+#     expect {
+#        "Would you like to create a user account now*" { send "y\r"; exp_continue }
+#         "Email*" { send "admin@admin.com\r"; exp_continue } 
+#         "Password*" { send "123456\r"; exp_continue }
+#         "Repeat for confirmation*" { send "123456\r"; exp_continue }
+#         "Should this user be a superuser*" { send "y\r" }
+#     }
+#     expect eof
+# -EOF
 
-echo '===== start sentry ====='
-sleep 2
-docker-compose up -d
+# echo '===== start sentry ====='
+# sleep 2
+# docker-compose up -d
 
 echo '===== building web server images ====='
 sleep 2
 cd ../
 docker-compose build
-docker-compose up -d
+# docker-compose up -d
 
 echo '========== End of building =========='
